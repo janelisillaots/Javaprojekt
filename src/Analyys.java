3fbb19151067d585fa99e84406cdffb1af8504fd
janelisillaots <janelisillaots@gmail.com>
2025-03-26 16:26:48 +0200

    Update Analyys.java

--- src/Analyys.java
+++ src/Analyys.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Analyys {
-}
+    private List<Integer> võidetudSummad;
+
+    public Analyys() {
+        this.võidetudSummad = new ArrayList<>();
+    }
+
+    public void võiduLisamine(int võit) {
+        võidetudSummad.add(võit);
+    }
+
+    public void väljastaVõidud() {
+        System.out.print("Sinu lotomänguga võidetud rahasummad: ");
+        for (int võit : this.võidetudSummad){
+            System.out.print(võit + " eurot, ");
+        }
+    }
