4490fa66aeae4f519b3cb605c763820e4b6388b0
janelisillaots <janelisillaots@gmail.com>
2025-03-28 11:21:32 +0200

    panin võidu listi ka lisama

--- src/Loos.java
+++ src/Loos.java
@@ -25,48 +25,52 @@ public class Loos {
             System.out.println("Peavõit! Arvasid kõik numbrid õiges järjekorras ära!");
             System.out.println("Võitsid 30 eurot."); //võidusumma, lisada võitudesse ja kontole
             konto.rahaSisse(30);
+            analyys.võiduLisamine(30);
 
         } else if (
                 (k1==n1 && k2==n3 && k3==n2) ||
-                (k1==n3 && k2==n2 && k3==n1) ||
-                (k1==n2 && k2==n1 && k3==n3) ||
-                (k1==n3 && k2==n1 && k3==n2) ||
-                (k1==n2 && k2==n3 && k3==n1)) {
+                        (k1==n3 && k2==n2 && k3==n1) ||
+                        (k1==n2 && k2==n1 && k3==n3) ||
+                        (k1==n3 && k2==n1 && k3==n2) ||
+                        (k1==n2 && k2==n3 && k3==n1)) {
             System.out.println("Võit! Arvasin kõik numbrid ära!");
             System.out.println("Võitsid 15 eurot."); //võidusumma, lisada võitudesse
             konto.rahaSisse(15);
+            analyys.võiduLisamine(15);
 
         } else if (
                 (k1==n1 && k2==n2) ||
-                (k1==n1 && k2==n3) ||
-                (k1==n2 && k2==n1) ||
-                (k1==n2 && k2==n3) ||
-                (k1==n3 && k2==n1) ||
-                (k1==n3 && k2==n2) ||
+                        (k1==n1 && k2==n3) ||
+                        (k1==n2 && k2==n1) ||
+                        (k1==n2 && k2==n3) ||
+                        (k1==n3 && k2==n1) ||
+                        (k1==n3 && k2==n2) ||
 
-                (k1==n1 && k3==n2) ||
-                (k1==n1 && k3==n3) ||
-                (k1==n2 && k3==n1) ||
-                (k1==n2 && k3==n3) ||
-                (k1==n3 && k3==n1) ||
-                (k1==n3 && k3==n2) ||
+                        (k1==n1 && k3==n2) ||
+                        (k1==n1 && k3==n3) ||
+                        (k1==n2 && k3==n1) ||
+                        (k1==n2 && k3==n3) ||
+                        (k1==n3 && k3==n1) ||
+                        (k1==n3 && k3==n2) ||
 
-                (k2==n1 && k3==n2) ||
-                (k2==n1 && k3==n3) ||
-                (k2==n2 && k3==n1) ||
-                (k2==n2 && k3==n3) ||
-                (k2==n3 && k3==n1) ||
-                (k2==n3 && k3==n2)) {
+                        (k2==n1 && k3==n2) ||
+                        (k2==n1 && k3==n3) ||
+                        (k2==n2 && k3==n1) ||
+                        (k2==n2 && k3==n3) ||
+                        (k2==n3 && k3==n1) ||
+                        (k2==n3 && k3==n2)) {
 
             System.out.println("Võit! Arvasid kaks numbrit ära!");
             System.out.println("Võitsid 5 eurot."); //võidusumma, lisada võitudesse
             konto.rahaSisse(5);
+            analyys.võiduLisamine(5);
 
         } else if (k1==n1 || k1==n2 || k1==n3 || k2==n1 || k2==n2 || k2==n3 || k3==n1 || k3==n2 || k3==n3) {
 
             System.out.println("Võit! Arvasid ühe numbri ära!");
             System.out.println("Võitsid 2 eurot."); //võidusumma, lisada võitudesse
             konto.rahaSisse(2);
+            analyys.võiduLisamine(2);
 
         } else {
             System.out.println("Kahjuks seekord ei võitnud!");
