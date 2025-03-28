5da6de6f01ce8ba04303a623732f4d426ce93af1
janelisillaots <janelisillaots@gmail.com>
2025-03-28 11:22:33 +0200

    Update Analyys.java

--- src/Analyys.java
+++ src/Analyys.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.max;
+
 public class Analyys {
     private List<Integer> võidetudSummad;
 
@@ -8,13 +10,26 @@ public class Analyys {
         this.võidetudSummad = new ArrayList<>();
     }
 
+    public List<Integer> getVõidetudSummad() {
+        return võidetudSummad;
+    }
+
     public void võiduLisamine(int võit) {
         võidetudSummad.add(võit);
     }
 
-    public void väljastaVõidud() {
-        System.out.print("Sinu lotomänguga võidetud rahasummad: ");
-        for (int võit : this.võidetudSummad){
-            System.out.print(võit + " eurot, ");
+    public int väljastaVõidud() { // väljastab võidetud summad eraldi, lisab ka võidetud kogusumma, väljastab ka suurima võidu, tagastab võidetud kogusumma
+        int koguSumma = 0;
+        System.out.print("Lotomänguga võidetud rahasummad: ");
+        for (int võit : this.võidetudSummad) {
+            System.out.print(võit + " eurot; ");
+            koguSumma += võit;
         }
+        System.out.println();
+        System.out.println("Suurim võitudest: " + max(this.võidetudSummad) + " eurot.");
+        System.out.println("Lotoga võidetud kogusumma: " + koguSumma + " eurot.");
+
+        return koguSumma;
     }
+
+}
